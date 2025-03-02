3d8fbb7f6371f93fae7932100f7a3e0289902091
Salman Rana <raosalmantnb@gmail.com>
2025-03-02 13:29:54 +0530

    first commit

---
+++ src/FunctionalComponents/Footer.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import "../FunctionalComponents/style.css" 
+
+export default class Footer extends Component {
+  render() {
+    return (
+      <div className='background text-light text-center p-2 mt-2'>
+        <p>copyright@mynewsapp.com</p>
+      </div>
+    )
+  }
+}
